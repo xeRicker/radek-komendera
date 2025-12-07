f6c8be70de8065909266438ecf0623ca033cb798
xeRicker <xericker@gmail.com>
2025-12-07 10:12:04 +0100

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const GITHUB_CONFIG = {
     TOKEN: '__GH_TOKEN__', 
-    OWNER: 'xeRicker', // Twój nick
-    REPO: 'xericker.github.io', // Nazwa repozytorium
-    BRANCH: 'main' // lub 'master'
+    OWNER: 'xeRicker',
+    REPO: 'radek-komendera',
+    BRANCH: 'main'
 };
